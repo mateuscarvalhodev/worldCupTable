135f4941b6273f81d1543e0361f5aa8465db021f
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2022-10-28 00:03:24 -0300

    get na Api e listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from 'react';
 import './App.css'
-import Navbar from './components/Navbar'
+import Main from './components/Main/Main'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    fetch("https://estagio.geopostenergy.com/WorldCup/GetAllTeams", {
+      headers: {
+        "content-Type": "application/json",
+        "git-user": "mateuscarvalhodev"
+      }
+  })
+  .then(response => response.json())
+  .then(data => {
+    setItems(data.Result)
+  })
+
+  .catch(() => {
+    error => console.log(error);
+  })
+  }, [])
   return (
    <>
-    <Navbar />
+   <ul>
+    {items.map(item => {
+      return (
+        <li key={item.Name}>
+          <strong>{item.Name}</strong>
+        </li>
+      )
+    })}
+   </ul>
+    {/* <Navbar />
+    <Main /> */}
    </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,144 @@
+import './style.css';
+export default function Main () {
+  return (
+    <>
+    <div className="texto">
+      <h3 className="tittle">Simulador da Copa do Mundo 2022</h3>
+      <span className="subtitle">Quem irá ao mata-mata? E quem levantará a taça? Dê seus palpites e compartilhe!</span>
+    </div>
+  <div className="container">
+    <div className="divisions">
+      <div className="groups">
+      <button>GRUPO A</button>
+      <ul>
+        <li>Catar CAT</li>
+        <li>Equador EQU</li>
+        <li>Senegal SEN</li>
+        <li>Holanda HOL</li>
+      </ul>
+      </div>
+    </div>
+    <div className="divisions">
+    <div className="groups">
+      <button>GRUPO B</button>
+      <ul>
+        <li>Inglaterra ING</li>
+        <li>Irã IRA</li>
+        <li>Estados Unidos EUA</li>
+        <li>País de Gales GAL</li>
+      </ul>
+      </div>
+    </div>
+    <div className="divisions">
+    <div className="groups">
+      <button>GRUPO C</button>
+      <ul>
+        <li>Argentina ARG</li>
+        <li>Arábia Saudita ABA</li>
+        <li>México MEX</li>
+        <li>Polônia POL</li>
+      </ul>
+      </div>
+    </div>
+    <div className="divisions">
+    <div className="groups">
+      <button>GRUPO D</button>
+      <ul>
+        <li>França FRA</li>
+        <li>Austrália AUS</li>
+        <li>Dinamarca DIN</li>
+        <li>Tunísia TUN</li>
+      </ul>
+      </div>
+    </div>
+    <div className="divisions">
+    <div className="groups">
+      <button>GRUPO E</button>
+      <ul>
+        <li>Espanha ESP</li>
+        <li>Costa Rica CRC</li>
+        <li>Alemanha ALE</li>
+        <li>Japão JAP</li>
+      </ul>
+      </div>
+    </div>
+    <div className="divisions">
+    <div className="groups">
+      <button>GRUPO F</button>
+      <ul>
+        <li>Bélgica BEL</li>
+        <li>Canadá CAN</li>
+        <li>Marrocos MAR</li>
+        <li>Croácia CRO</li>
+      </ul>
+      </div>
+    </div>
+    <div className="divisions">
+    <div className="groups">
+      <button>GRUPO G</button>
+      <ul>
+        <li>Brasil BRA</li>
+        <li>Sérvia SER</li>
+        <li>Suíça SUI</li>
+        <li>Camarôes CAM</li>
+      </ul>
+      </div>
+    </div>
+    <div className="divisions">
+    <div className="groups">
+      <button>GRUPO F</button>
+      <ul>
+        <li>Portugal POR</li>
+        <li>Gana GAN</li>
+        <li>Uruguai URU</li>
+        <li>Coreia do Sul COR</li>
+      </ul>
+      </div>
+    </div>
+    
+  </div>
+  
+  
+  
+  
+
+    </>
+  )
+  {/* <div className="texto">
+    <h3 className="tittle">Simulador da Copa do Mundo 2022</h3>
+    <span className="subtitle">Quem irá ao mata-mata? E quem levantará a taça? Dê seus palpites e compartilhe!</span>
+  </div>
+<div className="container">
+  <div className="divisions">
+    
+  </div>
+  <div className="divisions">
+    
+  </div>
+</div>
+<div className="container">
+  <div className="divisions">
+    
+  </div>
+  <div className="divisions">
+    
+  </div>
+</div>
+<div className="container">
+  <div className="divisions">
+    
+  </div>
+  <div className="divisions">
+    
+  </div>
+</div>
+<div className="container">
+  <div className="divisions">
+    
+  </div>
+  <div className="divisions">
+    
+  </div>
+</div>
+*/}
+}
